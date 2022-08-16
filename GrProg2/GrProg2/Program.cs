c3d1a0afb30643123de9c15029b5a0f0e97ba8a4
Lucas Kramhöft <110811527+LucasLupin@users.noreply.github.com>
2022-08-16 10:25:59 +0200

    Add files via upload
    Opgave

---
+++ GrProg2/GrProg2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace GrProg2
+{
+	public class Program
+	{
+		static void Main(string[ ] args)
+		{
+            string? playername = "";
+
+            while (playername != "exit")
+            {
+                Console.WriteLine("Hello");
+                playername = ChooseName();
+                if (playername != "exit" && playername != "")
+                 {
+                    SayMyName(playername);
+                }
+            }
+		}
+
+		static string? ChooseName()
+        {
+			Console.Write("Enter You´re name: ");
+			string? name = Console.ReadLine();
+			return name;
+
+        }
+
+		static void SayMyName(string? myName)
+        {
+            Console.WriteLine($"Hello {myName}. How are you doing?");
+
+        }
+	}
+}
+
